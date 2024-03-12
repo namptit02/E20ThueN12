58c741e37a2424a68b3af841e2f364855e8a1e65
namptit02 <namhoaq2k2@gmail.com>
2024-03-12 23:06:23 +0700

    update docker

--- src/main/java/com/example/hotrotinhthue/service/UserDetailServiceImp.java
+++ src/main/java/com/example/hotrotinhthue/service/UserDetailServiceImp.java
@@ -13,7 +13,8 @@ public class UserDetailServiceImp implements UserDetailsService {
     private NguoiDungService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername
+            (String username) throws UsernameNotFoundException {
         NguoiDung nguoiDung = userService.loadUserByUsername(username);
         return nguoiDung;
     }
